1dd19dc7e48e24db7ec79ea4bc34d9291ca31ac7
Srinath <srinath.a@techjays.com>
2021-09-16 19:41:56 +0530

    Text Intent added

--- mylibarry/src/main/java/com/example/mylibarry/ChatListActivity.kt
+++ mylibarry/src/main/java/com/example/mylibarry/ChatListActivity.kt
@@ -2,10 +2,24 @@ package com.example.mylibarry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class ChatListActivity : AppCompatActivity() {
+
+    lateinit var Text: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_list)
+
+        Text = findViewById(R.id.textView)
+
+        try {
+            var data = intent
+            var name = data.getStringExtra("name").toString()
+            Text.text = name
+        } catch (e: Exception) {
+            Log.d("ex", e.toString())
+        }
     }
 }
